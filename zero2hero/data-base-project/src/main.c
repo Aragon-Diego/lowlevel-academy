6358ef27d2827b832b6d5384c2fe32a86c333b2e
aragon-diego <diego.aragoncursos@gmail.com>
2025-09-18 23:06:35 -0600

    9th day

--- zero2hero/data-base-project/src/main.c
+++ zero2hero/data-base-project/src/main.c
@@ -20,6 +20,8 @@ int main(int argc, char *argv[]) {
   int c;
   char *filepath = NULL;
   bool newfile = false;
+  struct dbheader_t *dbhdr = NULL;
+
   while((c = getopt(argc, argv, "nf:")) != -1) {
     switch(c) {
       case 'n':
@@ -48,16 +50,23 @@ int main(int argc, char *argv[]) {
       printf("Unable to create database file\n");
       return -1;
     }
+    if(create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+      printf("Failed to create database header\n");
+      return -1;
+    }
   } else {
     dbfd = open_db_file(filepath);
     if(dbfd == STATUS_ERROR) {
       printf("Unable to open database file\n");
       return -1;
     }
+    if(validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+      printf("Failed to validate database header\n");
+      return -1;
+    }
   }
 
-  printf("Newfile: %d\n", newfile);
-  printf("Filepath: %s\n", filepath);
+  output_file(dbfd, dbhdr);
 
   return 0;
 }
